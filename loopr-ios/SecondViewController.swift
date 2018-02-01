0bb4812fa3cd57a9fc0b6c618d5f8c805f2308d1
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-01-31 19:01:12 -0800

    Create a new project.

---
+++ loopr-ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  loopr-ios
+//
+//  Created by xiaoruby on 1/31/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
