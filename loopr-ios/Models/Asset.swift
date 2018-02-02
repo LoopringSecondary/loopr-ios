b602f6aea6221cfc08d8e2cbc5ef7b60f338deb0
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-01 23:53:58 -0800

    Add Models

---
+++ loopr-ios/Models/Asset.swift
@@ -0,0 +1,21 @@
+//
+//  Asset.swift
+//  loopr-ios
+//
+//  Created by Xiao Dou Dou on 2/1/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import Foundation
+
+class Asset {
+    
+    let name: String
+    let enable: Bool
+    
+    init(name: String, enable: Bool) {
+        self.name = name
+        self.enable = enable
+    }
+    
+}
---
+++ loopr-ios/Models/Trade.swift
@@ -0,0 +1,19 @@
+//
+//  Trade.swift
+//  loopr-ios
+//
+//  Created by Xiao Dou Dou on 2/1/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import Foundation
+
+class Trade {
+    
+    let id: String
+    
+    init(id: String) {
+        self.id = id
+    }
+    
+}
