ec3ce4cfaa441cb73ae7bff5ed307e3c2c1693af
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-02 18:52:18 -0800

    Add MarketViewController.

---
+++ loopr-ios/Market/MarketTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MarketTableViewCell.swift
+//  loopr-ios
+//
+//  Created by Xiao Dou Dou on 2/2/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+class MarketTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    class func getHeight() -> CGFloat {
+        return 90
+    }
+}
---
+++ loopr-ios/Market/MarketViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MarketViewController.swift
+//  loopr-ios
+//
+//  Created by Xiao Dou Dou on 2/2/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+class MarketViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var marketTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        marketTableView.dataSource = self
+        marketTableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return MarketTableViewCell.getHeight()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "MarketCellIdentifier"
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? MarketTableViewCell
+        if (cell == nil) {
+            let nib = Bundle.main.loadNibNamed("MarketTableViewCell", owner: self, options: nil)
+            cell = nib![0] as? MarketTableViewCell
+            
+            // TODO: Tried to have a better animation when the user clicks the cell
+            cell?.selectionStyle = .none
+        }
+        
+        // Configure the cell...
+        return cell!
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
