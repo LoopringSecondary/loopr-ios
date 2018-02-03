4d190fec1172e5cc054e5296b6b5f740f6a20d33
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-03 10:39:46 -0800

    Add SettingViewController.

---
+++ loopr-ios/Setting/SettingNavigationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingNavigationViewController.swift
+//  loopr-ios
+//
+//  Created by Xiao Dou Dou on 2/3/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+class SettingNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let viewController = SettingViewController(nibName: nil, bundle: nil)
+        self.setViewControllers([viewController], animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ loopr-ios/Setting/SettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingViewController.swift
+//  loopr-ios
+//
+//  Created by Xiao Dou Dou on 2/3/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
