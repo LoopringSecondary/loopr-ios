8ab89fa4ff635b17eb3e23dfb04426fc2b4ede52
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-06 19:28:01 -0800

    Add drawLineChartView example.

--- loopr-ios/Market/MarketDetail/MarketDetailViewController.swift
+++ loopr-ios/Market/MarketDetail/MarketDetailViewController.swift
@@ -27,7 +27,7 @@ class MarketDetailViewController: UIViewController {
         lineChartViewController.view.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor, constant: 0.0).isActive = true
         lineChartViewController.view.leftAnchor.constraint(equalTo: mainScrollView.leftAnchor, constant: 0.0).isActive = true
         lineChartViewController.view.rightAnchor.constraint(equalTo: mainScrollView.rightAnchor, constant: 0.0).isActive = true
-        lineChartViewController.view.frame.size.height = 550
+        lineChartViewController.view.frame.size.height = 400
     }
 
     override func didReceiveMemoryWarning() {
--- loopr-ios/Market/MarketDetail/MarketLineChartViewController.swift
+++ loopr-ios/Market/MarketDetail/MarketLineChartViewController.swift
@@ -17,6 +17,7 @@ class MarketLineChartViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        drawLineChartView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +25,96 @@ class MarketLineChartViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func drawLineChartView() {
+        lineChartView.chartDescription?.enabled = false
+        lineChartView.dragEnabled = true
+        lineChartView.setScaleEnabled(false)
+        lineChartView.pinchZoomEnabled = false
+        lineChartView.drawGridBackgroundEnabled = false
+        
+        // x-axis limit line
+        let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
+        llXAxis.lineWidth = 4
+        llXAxis.lineDashLengths = [10, 10, 0]
+        llXAxis.labelPosition = .rightBottom
+        llXAxis.valueFont = .systemFont(ofSize: 10)
+        
+        lineChartView.xAxis.gridLineDashLengths = [10, 10]
+        lineChartView.xAxis.gridLineDashPhase = 0
+        
+        /*
+         let ll1 = ChartLimitLine(limit: 20, label: "Upper Limit")
+         ll1.lineWidth = 4
+         // ll1.lineDashLengths = [5, 5]
+         ll1.labelPosition = .rightTop
+         ll1.valueFont = .systemFont(ofSize: 10)
+         */
+        
+        let leftAxis = lineChartView.leftAxis
+        leftAxis.removeAllLimitLines()
+        // leftAxis.addLimitLine(ll1)
+        leftAxis.axisMaximum = 45
+        leftAxis.axisMinimum = 15
+        // leftAxis.gridLineDashLengths = [5, 5]
+        leftAxis.drawLimitLinesBehindDataEnabled = true
+        
+        lineChartView.rightAxis.enabled = false
+        
+        //[_chartView.viewPortHandler setMaximumScaleY: 2.f];
+        //[_chartView.viewPortHandler setMaximumScaleX: 2.f];
+        
+        lineChartView.legend.enabled = false // .form = .line
+        
+        // lineChartView.animate(xAxisDuration: 2.5)
+        
+        setDataCount(50, range: 10)
+        
+        for set in lineChartView.data!.dataSets as! [LineChartDataSet] {
+            set.mode = (set.mode == .cubicBezier) ? .horizontalBezier : .cubicBezier
+        }
+        lineChartView.setNeedsDisplay()
+    }
 
+    func setDataCount(_ count: Int, range: UInt32) {
+        let values = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(range) + 20) + Double(i) * 0.3
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        
+        let set1 = LineChartDataSet(values: values, label: "")
+        set1.drawIconsEnabled = false
+        
+        // set1.lineDashLengths = [5, 2.5]
+        set1.highlightLineDashLengths = [5, 2.5]
+        set1.setColor(UIColor(red: 34/255, green: 53/255, blue: 89/255, alpha: 1))
+        // set1.setCircleColor(.black)
+        set1.lineWidth = 2
+        set1.circleRadius = 0
+        set1.drawCircleHoleEnabled = false
+        set1.valueFont = .systemFont(ofSize: 0)
+        set1.formLineDashLengths = [5, 2.5]
+        set1.formLineWidth = 1
+        set1.formSize = 15
+        set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        
+        let gradientColors = [ChartColorTemplates.colorFromString("#005685df").cgColor,
+                              ChartColorTemplates.colorFromString("#ff080d16").cgColor]
+        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+        
+        set1.fillAlpha = 1
+        set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
+        // set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
+
+        // Enable the filled.
+        // set1.drawFilledEnabled = true
+        
+        let data = LineChartData(dataSet: set1)
+        
+        lineChartView.data = data
+        lineChartView.xAxis.enabled = false
+        lineChartView.leftAxis.enabled = false
+    }
+    
     /*
     // MARK: - Navigation
 
