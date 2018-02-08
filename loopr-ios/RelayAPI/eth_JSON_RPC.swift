573a575c8ab414e9387b64a185d8ff3b8cb4ee06
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-08 00:50:56 -0800

    Add eth_JSON_RPC

---
+++ loopr-ios/RelayAPI/eth_JSON_RPC.swift
@@ -0,0 +1,43 @@
+//
+//  eth_JSON_RPC.swift
+//  loopr-ios
+//
+//  Created by xiaoruby on 2/8/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class eth_JSON_RPC {
+    
+    // TODO:
+    public static func eth_call() {
+        
+    }
+
+    // TODO:
+    public static func eth_getTransactionCount() {
+        
+    }
+    
+    // TODO:
+    public static func eth_getBalance() {
+        
+    }
+    
+    // TODO:
+    public static func eth_gasPrice() {
+        
+    }
+    
+    // TODO:
+    public static func eth_sendRawTransaction() {
+        
+    }
+    
+    // TODO:
+    public static func eth_estimateGas() {
+        
+    }
+}
