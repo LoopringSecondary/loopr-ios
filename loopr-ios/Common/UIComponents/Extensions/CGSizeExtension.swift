a672aff51af7c4c83a566d46560023ba971bc904
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-08 22:04:46 -0800

    Add UIComponents/Extensions

---
+++ loopr-ios/Common/UIComponents/Extensions/CGSizeExtension.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  loopr-ios
+//
+//  Created by xiaoruby on 2/8/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+extension CGSize {
+    
+    func isZero() -> Bool {
+        return self.equalTo(CGSize())
+    }
+    
+}
