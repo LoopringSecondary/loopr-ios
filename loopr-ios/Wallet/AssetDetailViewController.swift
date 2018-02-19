9ed9f390a38875e2bda92aef95d70d68f88610c2
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-18 18:42:17 -0800

    Remove unused code in AssetDetailViewController.

--- loopr-ios/Wallet/AssetDetailViewController.swift
+++ loopr-ios/Wallet/AssetDetailViewController.swift
@@ -34,17 +34,6 @@ class AssetDetailViewController: UIViewController {
     
     @objc func pressQRCodeButton(_ button: UIBarButtonItem) {
         print("pressQRCodeButton")
-        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
