7c8ca451552f86e6435f40ce24557d384aa7a50c
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-19 22:03:18 -0800

    Add UITextFieldExtension

---
+++ loopr-ios/Common/UIComponents/Extensions/UITextFieldExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UITextFieldExtension.swift
+//  loopr-ios
+//
+//  Created by xiaoruby on 2/19/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}
+
