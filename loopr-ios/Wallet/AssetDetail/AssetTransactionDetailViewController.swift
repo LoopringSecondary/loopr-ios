a08699f15f47c8d149e6b271d7e8d588c3db8804
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-02-25 20:30:16 -0800

    Update UI color in AssetTransactionDetailViewController.swift

--- loopr-ios/Wallet/AssetDetail/AssetTransactionDetailViewController.swift
+++ loopr-ios/Wallet/AssetDetail/AssetTransactionDetailViewController.swift
@@ -30,6 +30,19 @@ class AssetTransactionDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.title = "Received"
         
+        view.theme_backgroundColor = ["#fff", "#000"]
+        typeImageView.theme_image = ["Received", "Received-white"]
+        amountLabel.theme_textColor = ["#000", "#fff"]
+        amountInCurrencyLabel.theme_textColor = ["#000", "#fff"]
+        label1.theme_textColor = ["#000", "#fff"]
+        label2.theme_textColor = ["#000", "#fff"]
+        label3.theme_textColor = ["#000", "#fff"]
+        label4.theme_textColor = ["#000", "#fff"]
+        label5.theme_textColor = ["#000", "#fff"]
+        label6.theme_textColor = ["#000", "#fff"]
+        label7.theme_textColor = ["#000", "#fff"]
+        label8.theme_textColor = ["#000", "#fff"]
+
         let backButton = UIBarButtonItem()
         backButton.title = ""
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
@@ -39,19 +52,19 @@ class AssetTransactionDetailViewController: UIViewController {
         super.viewWillAppear(animated)
         
         let line1 = DashedLineView(frame: CGRect(x: label1.frame.origin.x, y: label1.frame.origin.y + label1.frame.size.height * 0.5, width: self.view.frame.size.width - label1.frame.origin.x * 2, height: 1))
-        line1.lineColor = defaultTintColor
+        line1.lineColor = Themes.isNight() ? UIColor.white : defaultTintColor
         self.view.addSubview(line1)
 
         let line2 = DashedLineView(frame: CGRect(x: label3.frame.origin.x, y: label3.frame.origin.y + label3.frame.size.height * 0.5, width: self.view.frame.size.width - label3.frame.origin.x * 2, height: 1))
-        line2.lineColor = defaultTintColor
+        line2.lineColor = Themes.isNight() ? UIColor.white : defaultTintColor
         self.view.addSubview(line2)
         
         let line3 = DashedLineView(frame: CGRect(x: label5.frame.origin.x, y: label5.frame.origin.y + label5.frame.size.height * 0.5, width: self.view.frame.size.width - label5.frame.origin.x * 2, height: 1))
-        line3.lineColor = defaultTintColor
+        line3.lineColor = Themes.isNight() ? UIColor.white : defaultTintColor
         self.view.addSubview(line3)
         
         let line4 = DashedLineView(frame: CGRect(x: label7.frame.origin.x, y: label7.frame.origin.y + label7.frame.size.height * 0.5, width: self.view.frame.size.width - label7.frame.origin.x * 2, height: 1))
-        line4.lineColor = defaultTintColor
+        line4.lineColor = Themes.isNight() ? UIColor.white : defaultTintColor
         self.view.addSubview(line4)
     }
 
