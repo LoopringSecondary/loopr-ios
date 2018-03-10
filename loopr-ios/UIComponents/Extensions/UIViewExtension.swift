62c73263e72fe6fa6ec0ddf77f522eff6215d724
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-03-10 00:44:05 -0800

    Move Utils/UIView+Extension.swift to UIViewExtension.swift

--- loopr-ios/UIComponents/Extensions/UIViewExtension.swift
+++ loopr-ios/UIComponents/Extensions/UIViewExtension.swift
@@ -34,4 +34,55 @@ extension UIView {
         }
         return safeAreaInsets != .zero
     }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+            self.layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return self.layer.shadowRadius
+        }
+        set {
+            self.layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
 }
