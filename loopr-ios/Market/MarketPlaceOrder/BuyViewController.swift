3287cea8f15a0b273aa914f4ac9de756ff5d5fdc
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-03-10 17:15:48 -0800

    Add a scroll view in BuyViewController.

--- loopr-ios/Market/MarketPlaceOrder/BuyViewController.swift
+++ loopr-ios/Market/MarketPlaceOrder/BuyViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class BuyViewController: UIViewController {
+class BuyViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var placeOrderButton: UIButton!
+    
+    var tokenSLabel: UILabel = UILabel()
+    var tokenSAmountTextField: UITextField = UITextField()
+    var tokenSUnderLine: UIView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        placeOrderButton.title = NSLocalizedString("Place Order", comment: "")
+        placeOrderButton.backgroundColor = UIColor.black
+        placeOrderButton.layer.cornerRadius = 23
+        placeOrderButton.titleLabel?.font = UIFont(name: FontConfigManager.shared.getBold(), size: 16.0)
         
+        // Setup UI in the scroll view
+        let screensize: CGRect = UIScreen.main.bounds
+        let screenWidth = screensize.width
+        let screenHeight = screensize.height
+        
+        let originY: CGFloat = 50
+        let padding: CGFloat = 15
+        
+        tokenSLabel.text = PlaceOrderDataManager.shared.tokenS
+        tokenSLabel.font = FontConfigManager.shared.getLabelFont()
+        tokenSLabel.textAlignment = .right
+        // tokenSLabel.backgroundColor = UIColor.green
+        tokenSLabel.frame = CGRect(x: screenWidth-80-padding, y: originY, width: 80, height: 40)
+        scrollView.addSubview(tokenSLabel)
+        
+        tokenSAmountTextField.delegate = self
+        tokenSAmountTextField.inputView = UIView()
+        tokenSAmountTextField.font = FontConfigManager.shared.getLabelFont() // UIFont.init(name: FontConfigManager.shared.getLight(), size: 24)
+        // tokenSAmountTextField.backgroundColor = UIColor.blue
+        tokenSAmountTextField.placeholder = "Limited Price"
+        tokenSAmountTextField.contentMode = UIViewContentMode.bottom
+        tokenSAmountTextField.frame = CGRect(x: padding, y: originY, width: screenWidth-padding*2-80, height: 40)
+        scrollView.addSubview(tokenSAmountTextField)
+        
+        tokenSUnderLine.frame = CGRect(x: padding, y: tokenSLabel.frame.maxY, width: screenWidth - padding * 2, height: 1)
+        tokenSUnderLine.backgroundColor = UIColor.black
+        scrollView.addSubview(tokenSUnderLine)
+
+        scrollView.contentSize = CGSize(width: screenWidth, height: 2000)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func pressedPlaceOrderButton(_ sender: Any) {
+        print("pressedPlaceOrderButton")
+    }
+
+    /*
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        return false
+    }
+     */
 
 }
