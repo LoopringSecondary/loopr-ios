ef2519642a20e5cb847d61353f84b66705f0c040
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-03-10 19:20:01 -0800

    Layout several labels and text fields in BuyViewController.

--- loopr-ios/Market/MarketPlaceOrder/BuyViewController.swift
+++ loopr-ios/Market/MarketPlaceOrder/BuyViewController.swift
@@ -13,10 +13,24 @@ class BuyViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var placeOrderButton: UIButton!
     
+    // Price
     var tokenSLabel: UILabel = UILabel()
-    var tokenSAmountTextField: UITextField = UITextField()
+    var tokenSPriceTextField: UITextField = UITextField()
     var tokenSUnderLine: UIView = UIView()
+    var estimateValueInCurrency: UILabel = UILabel()
 
+    // Amout
+    var tokenBLabel: UILabel = UILabel()
+    var amountTextField: UITextField = UITextField()
+    var amountUnderLine: UIView = UIView()
+    var maxButton: UIButton = UIButton()
+
+    // Total
+    var tokenSTotalLabel: UILabel = UILabel()
+    var totalTextField: UITextField = UITextField()
+    var totalUnderLine: UIView = UIView()
+    var availableLabel: UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -35,6 +49,8 @@ class BuyViewController: UIViewController, UITextFieldDelegate {
         let originY: CGFloat = 50
         let padding: CGFloat = 15
         
+        // First row: price
+
         tokenSLabel.text = PlaceOrderDataManager.shared.tokenS
         tokenSLabel.font = FontConfigManager.shared.getLabelFont()
         tokenSLabel.textAlignment = .right
@@ -42,20 +58,85 @@ class BuyViewController: UIViewController, UITextFieldDelegate {
         tokenSLabel.frame = CGRect(x: screenWidth-80-padding, y: originY, width: 80, height: 40)
         scrollView.addSubview(tokenSLabel)
         
-        tokenSAmountTextField.delegate = self
-        tokenSAmountTextField.inputView = UIView()
-        tokenSAmountTextField.font = FontConfigManager.shared.getLabelFont() // UIFont.init(name: FontConfigManager.shared.getLight(), size: 24)
-        // tokenSAmountTextField.backgroundColor = UIColor.blue
-        tokenSAmountTextField.placeholder = "Limited Price"
-        tokenSAmountTextField.contentMode = UIViewContentMode.bottom
-        tokenSAmountTextField.frame = CGRect(x: padding, y: originY, width: screenWidth-padding*2-80, height: 40)
-        scrollView.addSubview(tokenSAmountTextField)
+        tokenSPriceTextField.delegate = self
+        tokenSPriceTextField.inputView = UIView()
+        tokenSPriceTextField.font = FontConfigManager.shared.getLabelFont() // UIFont.init(name: FontConfigManager.shared.getLight(), size: 24)
+        // tokenSPriceTextField.backgroundColor = UIColor.blue
+        tokenSPriceTextField.placeholder = "Limited Price"
+        tokenSPriceTextField.contentMode = UIViewContentMode.bottom
+        tokenSPriceTextField.frame = CGRect(x: padding, y: originY, width: screenWidth-padding*2-80, height: 40)
+        scrollView.addSubview(tokenSPriceTextField)
         
         tokenSUnderLine.frame = CGRect(x: padding, y: tokenSLabel.frame.maxY, width: screenWidth - padding * 2, height: 1)
         tokenSUnderLine.backgroundColor = UIColor.black
         scrollView.addSubview(tokenSUnderLine)
 
-        scrollView.contentSize = CGSize(width: screenWidth, height: 2000)
+        estimateValueInCurrency.text = "≈ $ 0.00"
+        estimateValueInCurrency.font = FontConfigManager.shared.getLabelFont()
+        estimateValueInCurrency.frame = CGRect(x: padding, y: tokenSUnderLine.frame.maxY, width: screenWidth-padding*2-80, height: 40)
+        // estimateValueInCurrency.backgroundColor = UIColor.green
+        scrollView.addSubview(estimateValueInCurrency)
+        
+        // Second row: amount
+        
+        tokenBLabel.text = PlaceOrderDataManager.shared.tokenB
+        tokenBLabel.font = FontConfigManager.shared.getLabelFont()
+        tokenBLabel.textAlignment = .right
+        // tokenBLabel.backgroundColor = UIColor.green
+        tokenBLabel.frame = CGRect(x: screenWidth-80-padding, y: estimateValueInCurrency.frame.maxY + 30, width: 80, height: 40)
+        scrollView.addSubview(tokenBLabel)
+        
+        amountTextField.delegate = self
+        amountTextField.inputView = UIView()
+        amountTextField.font = FontConfigManager.shared.getLabelFont() // UIFont.init(name: FontConfigManager.shared.getLight(), size: 24)
+        // amountTextField.backgroundColor = UIColor.blue
+        amountTextField.placeholder = "Amount"
+        amountTextField.contentMode = UIViewContentMode.bottom
+        amountTextField.frame = CGRect(x: padding, y: estimateValueInCurrency.frame.maxY + 30, width: screenWidth-padding*2-80, height: 40)
+        scrollView.addSubview(amountTextField)
+        
+        amountUnderLine.frame = CGRect(x: padding, y: tokenBLabel.frame.maxY, width: screenWidth - padding * 2, height: 1)
+        amountUnderLine.backgroundColor = UIColor.black
+        scrollView.addSubview(amountUnderLine)
+
+        maxButton.title = "Max"
+        maxButton.theme_setTitleColor(["#0094FF", "#000"], forState: .normal)
+        maxButton.setTitleColor(UIColor.init(rgba: "#cce9ff"), for: .highlighted)
+        maxButton.titleLabel?.font = FontConfigManager.shared.getLabelFont()
+        // maxButton.backgroundColor = UIColor.black
+        maxButton.contentHorizontalAlignment = .right
+        maxButton.frame = CGRect(x: screenWidth-80-padding, y: amountUnderLine.frame.maxY, width: 80, height: 40)
+        scrollView.addSubview(maxButton)
+        
+        // Thrid row: total
+        
+        tokenSTotalLabel.text = PlaceOrderDataManager.shared.tokenS
+        tokenSTotalLabel.font = FontConfigManager.shared.getLabelFont()
+        tokenSTotalLabel.textAlignment = .right
+        // tokenSTotalLabel.backgroundColor = UIColor.green
+        tokenSTotalLabel.frame = CGRect(x: screenWidth-80-padding, y: maxButton.frame.maxY + 30, width: 80, height: 40)
+        scrollView.addSubview(tokenSTotalLabel)
+        
+        totalTextField.delegate = self
+        totalTextField.inputView = UIView()
+        totalTextField.font = FontConfigManager.shared.getLabelFont() // UIFont.init(name: FontConfigManager.shared.getLight(), size: 24)
+        // amountTextField.backgroundColor = UIColor.blue
+        totalTextField.placeholder = "Total"
+        totalTextField.contentMode = UIViewContentMode.bottom
+        totalTextField.frame = CGRect(x: padding, y: maxButton.frame.maxY + 30, width: screenWidth-padding*2-80, height: 40)
+        scrollView.addSubview(totalTextField)
+        
+        totalUnderLine.frame = CGRect(x: padding, y: tokenSTotalLabel.frame.maxY, width: screenWidth - padding * 2, height: 1)
+        totalUnderLine.backgroundColor = UIColor.black
+        scrollView.addSubview(totalUnderLine)
+        
+        availableLabel.text = "Available 96.3236 ETH"
+        availableLabel.font = FontConfigManager.shared.getLabelFont()
+        availableLabel.frame = CGRect(x: padding, y: totalUnderLine.frame.maxY, width: screenWidth-padding*2-80, height: 40)
+        // estimateValueInCurrency.backgroundColor = UIColor.green
+        scrollView.addSubview(availableLabel)
+        
+        scrollView.contentSize = CGSize(width: screenWidth, height: availableLabel.frame.maxY + 30)
     }
 
     override func didReceiveMemoryWarning() {
@@ -67,11 +148,11 @@ class BuyViewController: UIViewController, UITextFieldDelegate {
         print("pressedPlaceOrderButton")
     }
 
-    /*
+
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing")
         
-        return false
+        return true
     }
-     */
 
 }
