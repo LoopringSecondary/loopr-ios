5949d34132a58b059729783ba61ca7f97e62acc3
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-04-11 00:20:17 -0700

    Add QR code image in DisplayKeystoreInQRCodeViewController

--- loopr-ios/Setting/SettingManageWallet/ExportKeystore/DisplayKeystoreInQRCodeViewController.swift
+++ loopr-ios/Setting/SettingManageWallet/ExportKeystore/DisplayKeystoreInQRCodeViewController.swift
@@ -10,17 +10,37 @@ import UIKit
 
 class DisplayKeystoreInQRCodeViewController: UIViewController {
 
+    @IBOutlet weak var qrcodeImageView: UIImageView!
+    var qrcodeImage: CIImage!
+    
     var appWallet: AppWallet!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let data = appWallet.getKeystore().description.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("Q", forKey: "inputCorrectionLevel")
+        qrcodeImage = filter!.outputImage
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Remove the blur effect
+        let scaleX = qrcodeImageView.frame.size.width / qrcodeImage.extent.size.width
+        let scaleY = qrcodeImageView.frame.size.height / qrcodeImage.extent.size.height
+        
+        let transformedImage = qrcodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+        qrcodeImageView.image = UIImage.init(ciImage: transformedImage)
     }
 
 }
