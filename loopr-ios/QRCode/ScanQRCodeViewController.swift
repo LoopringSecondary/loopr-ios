bcb5e04edf7e4a326515ed53e659188c5f5ae6df
Carina Shen <yimenglovesai@gmail.com>
2018-04-21 17:39:13 -0700

    added qr code reader for addresses.

--- loopr-ios/QRCode/ScanQRCodeViewController.swift
+++ loopr-ios/QRCode/ScanQRCodeViewController.swift
@@ -7,20 +7,107 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ScanQRCodeViewController: UIViewController {
-
+class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var scanView: UIView!
+    
+    var captureSession = AVCaptureSession()
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setBackButton()
         self.navigationItem.title = NSLocalizedString("QR Code", comment: "")
+        
+        // Get the camera for capturing videos
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
+        
+        print("devices: \(deviceDiscoverySession.devices)")
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera device")
+            return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession.addInput(input)
+
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        } catch {
+            print(error)
+            return
+        }
+        
+        // Initialize the video preview layer
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = self.scanView.layer.bounds
+        self.scanView.layer.addSublayer(videoPreviewLayer!)
+        
+        // Start video capture.
+        captureSession.startRunning()
+        
+        // Initialize QR Code Frame
+        qrCodeFrameView = UIView()
+        
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.black.cgColor
+            qrCodeFrameView.layer.borderWidth = 6
+            self.scanView.addSubview(qrCodeFrameView)
+            self.scanView.bringSubview(toFront: qrCodeFrameView)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func launchApp(decodedURL: String) {
+        
+        if presentedViewController != nil {
+            return
+        }
+        
+        let alertPrompt = UIAlertController(title: "Address detected", message: "\(decodedURL)", preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        alertPrompt.addAction(cancelAction)
+        
+        present(alertPrompt, animated: true, completion: nil)
+    }
 
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if [AVMetadataObject.ObjectType.qr].contains(metadataObj.type) {
+            // If the found metadata is equal to the QR code metadata (or barcode) then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            print("got scanned result: \(String(describing: metadataObj.stringValue))")
+            if metadataObj.stringValue != nil && metadataObj.stringValue!.starts(with: "0x") {
+                launchApp(decodedURL: metadataObj.stringValue!)
+            }
+        }
+    }
 }
