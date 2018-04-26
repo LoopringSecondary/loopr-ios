4ff5e9a6055ebd9eb4b0222fe23e4146af5fc757
yaqing <billwang1990@gmail.com>
2018-04-26 13:59:06 +0800

    [Yaqing] Fix the typo, rename KeystoneIntegrationTests to KeystoreIntegrationTests.

--- loopr-iosTests/Keystore/KeystoreIntegrationTests.swift
+++ loopr-iosTests/Keystore/KeystoreIntegrationTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import loopr_ios
 
-class KeystoneIntegrationTests: XCTestCase {
+class KeystoreIntegrationTests: XCTestCase {
     
     // Test dataset:
     // private key : public key
