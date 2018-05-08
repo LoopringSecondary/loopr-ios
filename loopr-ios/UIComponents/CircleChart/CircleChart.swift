9d5ab844b09b9db7682321c07d462bb1433df5cf
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-05-08 00:02:17 -0700

    Add background color to the circle chart.

--- loopr-ios/UIComponents/CircleChart/CircleChart.swift
+++ loopr-ios/UIComponents/CircleChart/CircleChart.swift
@@ -18,17 +18,18 @@ class CircleChart: UIView {
     
     var desiredLineWidth: CGFloat = 3
     var padding: CGFloat = 5
-    var clockwise: Bool = true
     var textColor: UIColor = UIColor.black
     var textFont: UIFont = UIFont.systemFont(ofSize: 14)
     var strokeColor: CGColor = UIColor.black.cgColor
     var fillColor: CGColor = UIColor.clear.cgColor
     
+    let backgroundShapeLayer = CAShapeLayer()
     let shapeLayer = CAShapeLayer()
     let label = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        layer.addSublayer(backgroundShapeLayer)
         layer.addSublayer(shapeLayer)
         addSubview(label)
     }
@@ -39,6 +40,7 @@ class CircleChart: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        layer.addSublayer(backgroundShapeLayer)
         layer.addSublayer(shapeLayer)
         addSubview(label)
     }
@@ -58,13 +60,22 @@ class CircleChart: UIView {
             radius: CGFloat( halfSize - (desiredLineWidth/2) - padding*2 ),
             startAngle: startAngle - CGFloat.pi*0.5,
             endAngle: endAngle * percentage - CGFloat.pi*0.5,
-            clockwise: clockwise)
-
+            clockwise: true)
         shapeLayer.path = circlePath.cgPath
-        
         shapeLayer.fillColor = fillColor
         shapeLayer.strokeColor = strokeColor
         shapeLayer.lineWidth = desiredLineWidth
+        
+        let circlePathInGrey = UIBezierPath(
+            arcCenter: CGPoint(x: halfSize, y: halfSize),
+            radius: CGFloat( halfSize - (desiredLineWidth/2) - padding*2 ),
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: false)
+        backgroundShapeLayer.path = circlePathInGrey.cgPath
+        backgroundShapeLayer.fillColor = fillColor
+        backgroundShapeLayer.strokeColor = UIColor.init(white: 0.0, alpha: 0.1).cgColor
+        backgroundShapeLayer.lineWidth = desiredLineWidth
     }
     
     internal func showTextInsdieView() {
