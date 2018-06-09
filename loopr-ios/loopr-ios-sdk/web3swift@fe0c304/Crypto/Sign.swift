06349244783d4db98d2ea5e4a4c71b8f86f83105
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-06-08 22:47:49 -0700

    Update hashedSignedMessage in rsv method

--- loopr-ios/loopr-ios-sdk/web3swift@fe0c304/Crypto/Sign.swift
+++ loopr-ios/loopr-ios-sdk/web3swift@fe0c304/Crypto/Sign.swift
@@ -23,7 +23,8 @@ open class Sign {
             _ = try? keystore.unlock(account, passphrase: passphrase)
             let accountAddress = account.getAddress()
             print("############### use address \(accountAddress?.getHex()) to sign")
-            let hashedSignedMessage = try keystore.signHash(accountAddress, hash: hashedMessage)
+            let hashedSignedMessage = try keystore.signHash(accountAddress, hash: secret)
+            // let hashedSignedMessage = try keystore.signHash(accountAddress, hash: hashedMessage)
             let r = hashedSignedMessage.subdata(in: Range(0..<32))
             let s = hashedSignedMessage.subdata(in: Range(32..<64))
             let v = hashedSignedMessage.subdata(in: Range(64..<65)) // TODO:- Use length
