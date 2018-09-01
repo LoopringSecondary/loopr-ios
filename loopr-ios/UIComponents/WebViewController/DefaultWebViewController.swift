e9764e72cf6d92d993bdb611a443fcaefb78e585
iwbrhwfy <kenshin_6@163.com>
2018-09-01 16:40:48 +0800

    complete safari feature

--- loopr-ios/UIComponents/WebViewController/DefaultWebViewController.swift
+++ loopr-ios/UIComponents/WebViewController/DefaultWebViewController.swift
@@ -12,12 +12,12 @@ import WebKit
 class DefaultWebViewController: UIViewController {
 
     @IBOutlet weak var wkWebView: WKWebView!
+    @IBOutlet weak var progressView: UIProgressView!
+
     var url: URL?
     var navigationTitle: String = ""
-    
-    private var progressKVOhandle: NSKeyValueObservation?
-    @IBOutlet weak var progressView: UIProgressView!
     var showProgressView: Bool = true
+    var progressKVOhandle: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,22 +31,36 @@ class DefaultWebViewController: UIViewController {
         progressView.setProgress(0, animated: false)
         progressView.alpha = 0.0
         
+        setupSafariButton()
+        
         let request = URLRequest(url: url!)
         wkWebView.navigationDelegate = self
         wkWebView.load(request)
         
         wkWebView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil) // add observer for key path
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupSafariButton() {
+        let safariButton = UIButton(type: UIButtonType.custom)
+        safariButton.setImage(UIImage(named: "Safari-item-button"), for: .normal)
+        safariButton.setImage(UIImage(named: "Safari-item-button")?.alpha(0.3), for: .highlighted)
+        safariButton.imageEdgeInsets = UIEdgeInsets.init(top: 0, left: 8, bottom: 0, right: -8)
+        safariButton.addTarget(self, action: #selector(pressedSafariButton(_:)), for: UIControlEvents.touchUpInside)
+        // The size of the image.
+        safariButton.frame = CGRect(x: 0, y: 0, width: 23, height: 23)
+        let shareBarButton = UIBarButtonItem(customView: safariButton)
+        self.navigationItem.rightBarButtonItem = shareBarButton
     }
     
+    @objc func pressedSafariButton(_ button: UIBarButtonItem) {
+        if let url = self.url {
+            UIApplication.shared.open(url)
+        }
+    }
+
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
         progressView.progress = Float(wkWebView.estimatedProgress)
     }
-
 }
 
 extension DefaultWebViewController: WKNavigationDelegate {
