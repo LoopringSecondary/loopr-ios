fa45076b0d4f1be343296161eded5a6a0b80ca80
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-09-29 15:20:03 -0700

    Fixed UIKit compiler errors.

--- loopr-ios/OrderHistory/HistoryDetail/OrderDetailViewController.swift
+++ loopr-ios/OrderHistory/HistoryDetail/OrderDetailViewController.swift
@@ -35,7 +35,7 @@ class OrderDetailViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var seperatorG: UIView!
     
     // Mask view
-    var blurVisualEffectView = UIView()
+    var blurVisualEffectView = UIView(frame: .zero)
     
     // Drag down to close a present view controller.
     var dismissInteractor = MiniToLargeViewInteractive()
@@ -45,12 +45,12 @@ class OrderDetailViewController: UIViewController, UIScrollViewDelegate {
     var tokenSView: TradeViewOnlyViewController = TradeViewOnlyViewController()
     var tokenBView: TradeViewOnlyViewController = TradeViewOnlyViewController()
     
-    var marketLabel: UILabel = UILabel()
-    var typeLabel: UILabel = UILabel()
+    var marketLabel: UILabel = UILabel(frame: .zero)
+    var typeLabel: UILabel = UILabel(frame: .zero)
     var qrcodeImageView: UIImageView!
     var qrcodeImage: UIImage!
-    var amountLabel: UILabel = UILabel()
-    var displayLabel: UILabel = UILabel()
+    var amountLabel: UILabel = UILabel(frame: .zero)
+    var displayLabel: UILabel = UILabel(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
