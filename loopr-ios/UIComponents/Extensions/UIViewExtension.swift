99f0ef33bdf611204a05ac07f3c10bb9fa4cc177
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-10-21 22:30:29 -0700

    Refactor GradientOrientation

--- loopr-ios/UIComponents/Extensions/UIViewExtension.swift
+++ loopr-ios/UIComponents/Extensions/UIViewExtension.swift
@@ -8,39 +8,6 @@
 
 import UIKit
 
-typealias GradientPoints = (startPoint: CGPoint, endPoint: CGPoint)
-
-enum GradientOrientation {
-    case bottomLeftTopRight
-    case topRightBottomLeft
-    case topLeftBottomRight
-    case horizontal
-    case vertical
-    
-    var startPoint: CGPoint {
-        return points.startPoint
-    }
-    
-    var endPoint: CGPoint {
-        return points.endPoint
-    }
-    
-    var points: GradientPoints {
-        switch self {
-        case .bottomLeftTopRight:
-            return (CGPoint(x: 0.0, y: 1), CGPoint(x: 1, y: 0.0))
-        case .topRightBottomLeft:
-            return (CGPoint(x: 0.0, y: 0.87), CGPoint(x: 0.87, y: 0.0))
-        case .topLeftBottomRight:
-            return (CGPoint(x: 0.0, y: 0.0), CGPoint(x: 1, y: 1))
-        case .horizontal:
-            return (CGPoint(x: 0.0, y: 0.5), CGPoint(x: 1.0, y: 0.5))
-        case .vertical:
-            return (CGPoint(x: 0.0, y: 0.0), CGPoint(x: 0.0, y: 1.0))
-        }
-    }
-}
-
 extension UIView {
     
     @discardableResult
