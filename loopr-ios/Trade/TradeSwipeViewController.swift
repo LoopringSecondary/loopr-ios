bc57da5ec30edfed09b240f7de7ebe605d219b9e
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-10-29 01:15:58 -0700

    Add a scan button in TradeSwipeViewController.

--- loopr-ios/Trade/TradeSwipeViewController.swift
+++ loopr-ios/Trade/TradeSwipeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TradeSwipeViewController: SwipeViewController {
+class TradeSwipeViewController: SwipeViewController, QRCodeScanProtocol {
     
     private var type: TradeSwipeType = .trade
     private var types: [TradeSwipeType] = []
@@ -24,6 +24,7 @@ class TradeSwipeViewController: SwipeViewController {
         view.theme_backgroundColor = ColorPicker.backgroundColor
         navigationItem.title = LocalizedString("P2P Trade", comment: "")
         setBackButton()
+        setNavigationBarItem()
         setupChildViewControllers()
         
         // Get the updated nonce
@@ -45,6 +46,22 @@ class TradeSwipeViewController: SwipeViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setNavigationBarItem() {
+        let icon = UIImage.init(named: "dropdown-scan")
+        let button = UIBarButtonItem(image: icon, style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.pressedScanButton))
+        self.navigationItem.rightBarButtonItem = button
+    }
+    
+    @objc func pressedScanButton() {
+        let viewController = ScanQRCodeViewController()
+        // TODO: do we need to support these types
+        viewController.expectedQRCodeTypes = [.submitOrder, .login, .cancelOrder, .convert, .approve, .p2pOrder, .address]
+        viewController.delegate = self
+        viewController.shouldPop = false
+        viewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
     
     func setupChildViewControllers() {
@@ -83,4 +100,89 @@ class TradeSwipeViewController: SwipeViewController {
     override func swipeView(_ swipeView: SwipeView, viewControllerForPageAt index: Int) -> UIViewController {
         return viewControllers[index]
     }
+    
+    // Copy code from WalletViewController.
+    func setResultOfScanningQRCode(valueSent: String, type: QRCodeType) {
+        let manager = AuthorizeDataManager.shared
+        if let data = valueSent.data(using: .utf8) {
+            let json = JSON(data)
+            switch type {
+            case .submitOrder:
+                manager.submitHash = json["value"].stringValue
+                manager.getSubmitOrder { (_, error) in
+                    guard error == nil, let order = manager.submitOrder else { return }
+                    DispatchQueue.main.async {
+                        let vc = PlaceOrderConfirmationViewController()
+                        vc.view.theme_backgroundColor = ColorPicker.backgroundColor
+                        vc.order = order
+                        vc.isSigning = true
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                }
+            case .login:
+                manager.loginUUID = json["value"].stringValue
+                manager._authorizeLogin { (_, error) in
+                    let result = error == nil ? true : false
+                    DispatchQueue.main.async {
+                        let vc = LoginResultViewController()
+                        vc.result = result
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                }
+            case .cancelOrder:
+                manager.cancelHash = json["value"].stringValue
+                manager.getCancelOrder { (_, error) in
+                    let result = error == nil ? true : false
+                    DispatchQueue.main.async {
+                        let vc = LoginResultViewController()
+                        vc.result = result
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                }
+            case .convert:
+                manager.convertHash = json["value"].stringValue
+                manager.getConvertTx { (_, error) in
+                    let result = error == nil ? true : false
+                    DispatchQueue.main.async {
+                        let vc = LoginResultViewController()
+                        vc.result = result
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                }
+                
+            case .approve:
+                manager.approveHash = json["value"].stringValue
+                manager.getApproveTxs { (_, error) in
+                    let result = error == nil ? true : false
+                    DispatchQueue.main.async {
+                        let vc = LoginResultViewController()
+                        vc.result = result
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                }
+            case .p2pOrder:
+                TradeDataManager.shared.handleResult(of: json["value"])
+                let vc = TradeConfirmationViewController()
+                vc.view.theme_backgroundColor = ColorPicker.backgroundColor
+                vc.parentNavController = self.navigationController
+                vc.order = TradeDataManager.shared.orders[1]
+                self.navigationController?.pushViewController(vc, animated: true)
+                
+            case .address:
+                let vc = SendAssetViewController()
+                vc.address = valueSent
+                vc.hidesBottomBarWhenPushed = true
+                self.navigationController?.pushViewController(vc, animated: true)
+                
+            case .keystore, .mnemonic, .privateKey:
+                let vc = UnlockWalletSwipeViewController()
+                vc.hidesBottomBarWhenPushed = true
+                vc.setResultOfScanningQRCode(valueSent: valueSent, type: type)
+                self.navigationController?.pushViewController(vc, animated: true)
+            default:
+                return
+            }
+        }
+    }
+
 }
