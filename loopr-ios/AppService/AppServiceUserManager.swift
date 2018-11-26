d7356f0e990bca1919bd72010d8da9683ce0b5a1
iwbrhwfy <kenshin_6@163.com>
2018-11-26 15:21:55 +0800

    fix bug

--- loopr-ios/AppService/AppServiceUserManager.swift
+++ loopr-ios/AppService/AppServiceUserManager.swift
@@ -12,7 +12,7 @@ class AppServiceUserManager {
     
     static let shared = AppServiceUserManager()
     
-    let BAK_URL = "https://www.loopring.mobi/rpc/v1/user"
+    let BAK_URL = "https://www.loopring.mobi/rpc/v1/user/"
     
     let GET_URL, ADD_URL, DEL_URL: String
 
